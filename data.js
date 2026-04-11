5507e27ea86332b6b2e796f027ed2fd6fe1f173e
diler-fivelife-mapka <mapkapoddilera@gmail.com>
2026-04-11 23:54:28 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,5 @@
+const markers = [
+  { name: "Warsztat Centralny", lat: 500, lng: 500, color: "red" },
+  { name: "NPC 1", lat: 300, lng: 700, color: "blue" },
+  { name: "Znajdźka 1", lat: 800, lng: 200, color: "green" }
+];
